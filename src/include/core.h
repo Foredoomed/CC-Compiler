b26835c753f8a79f6a4aa2c1c88aa1009c6fc8ef
Foredoomed <mc.liuxuan@gmail.com>
2013-06-01 09:45:36 +0800

    Change file structure

---
+++ src/include/core.h
@@ -0,0 +1,40 @@
+/***********************************************************************
+**
+**  Clean C
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**  http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+************************************************************************/
+
+
+#ifndef CORE_H
+#define CORE_H
+
+#include <stdint.h>
+
+
+typedef int8_t BYTE;
+typedef int16_t SHORT;
+typedef int32_t INT;
+typedef int64_t LONG;
+typedef float FLOAT;
+typedef double DOUBLE;
+typedef int16_t CHAR;
+typedef int BOOL;
+
+
+#define true 1
+#define false 0
+
+
+#endif
---
+++ src/include/lexer.h
@@ -0,0 +1,49 @@
+/***********************************************************************
+**
+**  Clean C
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**  http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+************************************************************************/
+
+
+#ifndef LEXER_H
+#define LEXER_H
+
+#include "core.h"
+
+
+BOOL init(const char *file);
+
+BOOL next();
+
+char *get_token();
+
+BOOL is_litteral();
+
+BOOL is_left();
+
+BOOL is_right();
+
+BOOL is_stop();
+
+BOOL is_parenthesis();
+
+BOOL is_call();
+
+int is_alpha();
+
+BOOL is_assignment();
+
+
+#endif
